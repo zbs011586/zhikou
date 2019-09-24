7404daaf7c7354f1974dba9f4aff14c1caafce70
zbs <zbs011586@163.com>
2019-09-24 21:22:04 +0800

    code

--- src/main/java/com/zhikou/code/commons/MvcConfig.java
+++ src/main/java/com/zhikou/code/commons/MvcConfig.java
@@ -1,11 +1,27 @@
 package com.zhikou.code.commons;
 
+import com.zhikou.code.interceptor.TokenInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private TokenInterceptor tokenInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        InterceptorRegistration addInterceptor = registry.addInterceptor(tokenInterceptor);
+        // 排除配置
+        addInterceptor.excludePathPatterns("/image/**");//排除静态资源
+        // 拦截配置
+        addInterceptor.addPathPatterns("/**");
+    }
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
