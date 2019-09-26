8ba4a36c6fdaa860644287fceb4f7502a56dcefa
zbs <zbs011586@163.com>
2019-09-26 20:22:43 +0800

    code

--- src/main/java/com/zhikou/code/dao/ShopDao.java
+++ src/main/java/com/zhikou/code/dao/ShopDao.java
@@ -1,8 +1,12 @@
 package com.zhikou.code.dao;
 
 import com.zhikou.code.bean.Shop;
+import org.apache.ibatis.annotations.Param;
 import tk.mybatis.mapper.common.Mapper;
+
+import java.util.List;
 
 public interface ShopDao extends Mapper<Shop> {
 
+    List<Shop> selectByLonLat(@Param("minLon")double minLon,@Param("maxLon")double maxLon,@Param("minLat")double minLat,@Param("maxLat")double maxLat);
 }
