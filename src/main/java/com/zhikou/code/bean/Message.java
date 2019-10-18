201b5d8a48e37825d99b73106e98a57193c46b9d
zbs <zbs011586@163.com>
2019-10-18 11:45:14 +0800

    code

--- src/main/java/com/zhikou/code/bean/Message.java
+++ src/main/java/com/zhikou/code/bean/Message.java
@@ -65,7 +65,7 @@ public class Message {
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date createTime;//创建时间
 
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private Date startTime;//活动开始时间
 
     @Transient
